4199576671d2583757c75f3dc0ad9070c7d9408a
bean <jump09205@gmail.com>
2025-05-17 21:55:23 +0800

    footer jp

---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+const Footer = () => {
+  return (
+    <div>
+      <footer id="globalFooter" class="sf-footer"></footer>
+      <script src="//www.muji.com/js/include-header-footer.js"></script>
+    </div>
+  );
+};
+
+export default Footer;
