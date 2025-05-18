6e541131d41f2ac067b21f65c65af5e0a7ba9135
bean <jump09205@gmail.com>
2025-05-18 18:19:30 +0800

    fix footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,20 @@
+import { useEffect } from "react";
+
 const Footer = () => {
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src = "//www.muji.com/js/include-header-footer.js";
+    script.async = true;
+    document.body.appendChild(script);
+
+    return () => {
+      document.body.removeChild(script); // 清除 script
+    };
+  }, []);
+
   return (
     <div>
-      <footer id="globalFooter" class="sf-footer"></footer>
-
+      <footer id="globalFooter" className="sf-footer"></footer>
     </div>
   );
 };
